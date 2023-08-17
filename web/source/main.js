9e51abde9bfc3c15768fa76aa50192f6e247f19c
Jan-Krystof Zahradnik <jendazah@gmail.com>
2023-08-17 20:34:53 +0200

    ee

--- web/source/main.js
+++ web/source/main.js
@@ -12,10 +12,9 @@ function showMenu(){
     }
 }
 
-function loading(element){
+function printAndLoad(element){
     const articles = document.getElementsByClassName("article");
     const description = document.getElementsByClassName("description-title");
-    // const date = document.getElementsByClassName("date");
     const getDesc = element.firstElementChild.innerText.split("\n");
    
     element_title = getDesc[1];
@@ -24,12 +23,10 @@ function loading(element){
     read_length_date = read_length[0]
     read_length_length = read_length[1]
 
-
     let height_sum = 0;
     for (let i = 0; i < articles.length; i++) {
         height_sum += articles[i].offsetHeight;
     }
-    
     let desc_length = 0;
     for (let i = 0; i < document.getElementsByClassName("date").length; i++) {
         desc_length+=parseInt(document.getElementsByClassName("date")[i].innerHTML.split(" / ")[1].split(" ")[0])
@@ -38,13 +35,17 @@ function loading(element){
     for (let i = 0; i < description.length; i++) {
         desc_2+=description[i].innerHTML.length;
     }
+
     console.log("Počet příspěvků: " + articles.length);
     console.log("Průměrná výška příspěvků: " + height_sum/articles.length);
     // MAYBE ?
     console.log("Součet délek nadpisů příspěvků (prvni delka charakteru, pak delka minut): " + desc_2 +", "+desc_length);
     console.log("název; datum; délku: " +element_title + "; " + read_length_date + "; " + read_length_length);
     
+     loading();
+}
+
+function loading(){
     document.getElementById("loading-screen").style.display = "flex";
-    setTimeout(() => {document.getElementById("loading-screen").style.display = "none";}, 1000);
-    
+    setTimeout(() => {document.getElementById("loading-screen").style.display = "none";}, 1000);  
 }
